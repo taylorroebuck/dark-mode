0d7aee54f9757d5cc01ed21f5aa22fe2c67c56f1
Taylor Roebuck <taylornroebuck@gmail.com>
2020-03-19 18:16:02 -0700

    fixed useDarkMode hook, achieved MVP

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,13 +1,15 @@
 import { useEffect } from 'react';
 import { useLocalStorage } from './useLocalStorage';
 
-export const useDarkMode = () => {
-    const [darkMode, setDarkMode] = useLocalStorage('dark', false);
+export const useDarkMode = initialValue => {
+    const [darkMode, setDarkMode] = useLocalStorage('dark-mode', initialValue);
+
     useEffect(() => {
-        const body = document.getElementById('bodyTag');
-        darkMode
-            ? body.classList.add('dark-mode')
-            : body.classList.remove('dark-mode');
+      const body = document.querySelector('body');
+      darkMode
+        ? body.classList.add('dark-mode')
+        : body.classList.remove('dark-mode');
     }, [darkMode]);
+
     return [darkMode, setDarkMode];
-};
+  };
